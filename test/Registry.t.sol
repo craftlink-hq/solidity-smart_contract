048336465a0fb3655c565ca9d39d4691a5c3e5e1
pope-h <hamatthan@gmail.com>
2025-06-08 18:03:54 +0100

    feat: test for registry

---
+++ test/Registry.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/v2/Registry.sol";
+
+contract RegistryTest is Test {
+    Registry registry;
+    address relayer = address(0x1);
+    address user1 = address(0x2);
+    address user2 = address(0x3);
+
+    function setUp() public {
+        registry = new Registry(relayer);
+    }
+
+    function testRegisterAsArtisan() public {
+        vm.prank(user1);
+        registry.registerAsArtisan("ipfsHash1");
+        assertEq(uint(registry.userTypes(user1)), uint(Registry.UserType.Artisan));
+        (string memory ipfsHash,,) = registry.getArtisanDetails(user1);
+        assertEq(ipfsHash, "ipfsHash1");
+    }
+
+    function testCannotRegisterTwice() public {
+        vm.startPrank(user1);
+        registry.registerAsArtisan("ipfsHash1");
+        vm.expectRevert("User already registered as an artisan");
+        registry.registerAsArtisan("ipfsHash2");
+        vm.stopPrank();
+    }
+
+    function testRelayerRegisterFor() public {
+        vm.prank(relayer);
+        registry.registerAsArtisanFor(user2, "ipfsHash2");
+        assertEq(uint(registry.userTypes(user2)), uint(Registry.UserType.Artisan));
+    }
+
+    function testNonRelayerCannotRegisterFor() public {
+        vm.prank(user1);
+        vm.expectRevert("Caller is not the relayer");
+        registry.registerAsArtisanFor(user2, "ipfsHash2");
+    }
+}
