abf8590e2ff7697ecff74bb64ac1d26daf5ee9f2
pope-h <hamatthan@gmail.com>
2025-06-08 18:29:25 +0100

    feat: added implementation for chat system

---
+++ test/ChatSystem.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/v2/ChatSystem.sol";
+import "../src/v2/GigMarketplace.sol";
+import "../src/v2/Registry.sol";
+import "../src/v2/PaymentProcessor.sol";
+import "../src/v2/Token.sol";
+
+contract ChatSystemTest is Test {
+    Registry registry;
+    Token token;
+    PaymentProcessor paymentProcessor;
+    GigMarketplace gigMarketplace;
+    ChatSystem chatSystem;
+    address relayer = address(0x1);
+    address client = address(0x2);
+    address artisan = address(0x3);
+    bytes32 databaseId = keccak256("databaseId");
+    bytes32 conversationId = keccak256("conversationId");
+
+    function setUp() public {
+        registry = new Registry(relayer);
+        token = new Token(relayer);
+        paymentProcessor = new PaymentProcessor(relayer, address(token));
+        gigMarketplace = new GigMarketplace(relayer, address(registry), address(paymentProcessor));
+        chatSystem = new ChatSystem(address(gigMarketplace));
+        vm.prank(client);
+        registry.registerAsClient("clientIpfs");
+        vm.prank(artisan);
+        registry.registerAsArtisan("artisanIpfs");
+        vm.prank(client);
+        token.claim();
+        vm.prank(client);
+        token.approve(address(paymentProcessor), 1000 * 10**6);
+        vm.prank(client);
+        gigMarketplace.createGig(keccak256("rootHash"), databaseId, 100 * 10**6);
+        vm.prank(artisan);
+        gigMarketplace.applyForGig(databaseId);
+        vm.prank(client);
+        gigMarketplace.hireArtisan(databaseId, artisan);
+    }
+
+    function testStartConversation() public {
+        vm.prank(client);
+        chatSystem.startConversation(conversationId, databaseId, keccak256("initialHash"));
+        (bytes32 rootHash, bool isActive) = chatSystem.getConversationDetails(conversationId);
+        assertEq(rootHash, keccak256("initialHash"));
+        assertTrue(isActive);
+    }
+
+    function testUpdateConversation() public {
+        vm.prank(client);
+        chatSystem.startConversation(conversationId, databaseId, keccak256("initialHash"));
+        vm.prank(artisan);
+        chatSystem.updateConversation(conversationId, databaseId, keccak256("newHash"));
+        (bytes32 rootHash,) = chatSystem.getConversationDetails(conversationId);
+        assertEq(rootHash, keccak256("newHash"));
+    }
+}
