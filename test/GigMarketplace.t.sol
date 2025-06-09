3c0565b50b58eda58bae2d371cfb1935fee12585
pope-h <hamatthan@gmail.com>
2025-06-09 18:53:12 +0100

    feat: started test for gig marketplace

--- test/GigMarketplace.t.sol
+++ test/GigMarketplace.t.sol
@@ -14,7 +14,9 @@ contract GigMarketplaceTest is Test {
     GigMarketplace gigMarketplace;
     address relayer = address(0x1);
     address client = address(0x2);
-    address artisan = address(0x3);
+    address client2 = address(0x3);
+    address artisan = address(0x4);
+    address artisan2 = address(0x5);
     bytes32 databaseId = keccak256("databaseId");
 
     function setUp() public {
@@ -24,11 +26,19 @@ contract GigMarketplaceTest is Test {
         gigMarketplace = new GigMarketplace(relayer, address(registry), address(paymentProcessor));
         vm.prank(client);
         registry.registerAsClient("clientIpfs");
+        vm.prank(client2);
+        registry.registerAsClient("client2Ipfs");
         vm.prank(artisan);
         registry.registerAsArtisan("artisanIpfs");
+        vm.prank(artisan2);
+        registry.registerAsArtisan("artisan2Ipfs");
         vm.prank(client);
         token.claim();
+        vm.prank(client2);
+        token.claim();
         vm.prank(client);
+        token.approve(address(paymentProcessor), 1000 * 10 ** 6);
+        vm.prank(client2);
         token.approve(address(paymentProcessor), 1000 * 10 ** 6);
     }
 
@@ -37,6 +47,36 @@ contract GigMarketplaceTest is Test {
         gigMarketplace.createGig(keccak256("rootHash"), databaseId, 100 * 10 ** 6);
         (address gigClient,,,,,,) = gigMarketplace.getGigInfo(databaseId);
         assertEq(gigClient, client);
+    }
+
+    function testCreateGigFor() public {
+        vm.prank(relayer);
+        gigMarketplace.createGigFor(client, keccak256("rootHash"), databaseId, 100 * 10 ** 6);
+        (address gigClient,,,,,,) = gigMarketplace.getGigInfo(databaseId);
+        assertEq(gigClient, client);
+    }
+
+    function testNonRelayerCannotCreateGigFor() public {
+        vm.prank(artisan);
+        vm.expectRevert("Caller is not the relayer");
+        gigMarketplace.createGigFor(client, keccak256("rootHash"), databaseId, 100 * 10 ** 6);
+    }
+
+    function testNonClientCannotCreateGig() public {
+        vm.prank(artisan);
+        vm.expectRevert("Not a client");
+        gigMarketplace.createGig(keccak256("rootHash"), databaseId, 100 * 10 ** 6);
+    }
+
+    function testCreateMultipleGigFor() public {
+        vm.startPrank(relayer);
+        gigMarketplace.createGigFor(client, keccak256("rootHash1"), keccak256("databaseId1"), 100 * 10 ** 6);
+        gigMarketplace.createGigFor(client2, keccak256("rootHash2"), keccak256("databaseId2"), 200 * 10 ** 6);
+        vm.stopPrank();
+        (address gigClient1,,,,,,) = gigMarketplace.getGigInfo(keccak256("databaseId1"));
+        (address gigClient2,,,,,,) = gigMarketplace.getGigInfo(keccak256("databaseId2"));
+        assertEq(gigClient1, client);
+        assertEq(gigClient2, client2);
     }
 
     function testApplyForGig() public {
