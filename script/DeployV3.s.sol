7c5175af75be87f9b19ef633ac2e04fdb5bbec90
pope-h <hamatthan@gmail.com>
2025-10-23 14:13:40 +0100

    added deployment line

--- script/DeployV3.s.sol
+++ script/DeployV3.s.sol
@@ -57,3 +57,5 @@ contract DeployV3Script is Script {
 
 // DEPLOY COMMAND
 // forge script script/DeployV3.s.sol:DeployV3Script --rpc-url $LISK_SEPOLIA_RPC_URL --broadcast --verify -vvvv
+
+// forge script script/DeployV3.s.sol:DeployV3Script --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --verify --verifier etherscan -vvvv
